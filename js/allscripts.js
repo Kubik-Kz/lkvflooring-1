9f8ca9953aa969ac153a805c04559f97995b768d
Kubik-Kz <47062547+Kubik-Kz@users.noreply.github.com>
2019-02-17 17:51:55 +0600

    Change

--- js/allscripts.js
+++ js/allscripts.js
@@ -146,3 +146,14 @@ $(".form-success-close").click(function(){
    $(".success").addClass("close-form"); 
 });
 
+
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+}); 
